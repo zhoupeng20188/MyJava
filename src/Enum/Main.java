cfafb9de681bd2617f10eb699dab461898772d26
zhoupeng20188 <287214764@qq.com>
2019-10-29 19:39:48 +0800

    Enum定数

---
+++ src/Enum/Main.java
@@ -0,0 +1,9 @@
+package Enum;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Apple.富士);
+        System.out.println(Car.SUV.getCode());
+        System.out.println(Car.SUV.name());
+    }
+}

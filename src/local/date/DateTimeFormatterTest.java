37ccd510174aef7b58d36247a6722f0d3d25f27d
zhoupeng <287214764@qq.com>
2020-11-10 17:40:44 +0800

    LocalDateTime转换测试

--- src/local/date/DateTimeFormatterTest.java
+++ src/local/date/DateTimeFormatterTest.java
@@ -13,7 +13,14 @@ public class DateTimeFormatterTest {
         LocalDateTime now = LocalDateTime.now();
         String format = now.format(df);
         System.out.println(format);
+        // 将LocalDateTime格式为str
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy年MM月dd日 HH:mm:ss");
         System.out.println(now.format(dtf));
+        String str = dtf.format(now);
+        System.out.println(str);
+        // 将str转回为LocalDateTime
+        LocalDateTime parse = LocalDateTime.parse(str, dtf);
+        System.out.println(parse);
+
     }
 }
